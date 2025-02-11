adf413c221f8fa5e439d42130173479b2fd24207
Petri <dimtri.williams.6448@gmail.com>
2025-02-10 18:12:15 -0600

    Feat/pwa: Finished setting up service worker so applcation can 100% work off line and delete all cache when app is first activated.
    * Updated cached values to use absolute paths.
    
    * Old cache deleted during the activate phase of PWA.
    
    * PWA works 100% offline
    	- any navigate response will navigate back to index.html.
    	- All other responses will check cache and if nothing is found
    	  return a 404 not found status.

--- src/sw.js
+++ src/sw.js
@@ -3,17 +3,54 @@ const CACHE_NAME = `todo-cache-${VERSION}`;
 
 const APP_STATIC_RESOURCES = [
   "/",
-  "./index.html",
-  "./scripts/bundle.js",
-  "./css/styles.css",
-  "./icons/todo.svg",
-  "./img/chromatic.png",
-  "./img/shiba.png",
-  "./manifest.webmanifest",
+  "/index.html",
+  "/scripts/bundle.js",
+  "/css/styles.css",
+  "/icons/todo.svg",
+  "/img/chromatic.png",
+  "/img/shiba.png",
+  "/manifest.webmanifest",
 ];
 
 // Event Listners
 self.addEventListener("install", installApp);
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(async () => {
+    const names = await caches.keys();
+
+    await Promise.all(
+      names.map((name) => {
+        if (name !== CACHE_NAME) {
+          return caches.delete(name);
+        }
+      })
+    );
+
+    await clients.claim();
+  })();
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.mode === "navigate") {
+    e.respondWith(caches.match("./"));
+    return;
+  }
+
+  e.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      const cachedResponse = await cache.match(e.request.url);
+
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return new Response(null, { status: 404 });
+    })()
+  );
+});
 
 // Core Functions
 function installApp(e) {
