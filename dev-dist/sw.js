d1f56778cd1c2dc8d1e926f8b933cb39cc6b737b
Petri <dimtri.williams.6448@gmail.com>
2025-05-23 21:42:43 -0500

    Refact/vite-conversion: Removed .babelrc.js as that was only needed for webpack.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.boe06sben38"
+    "revision": "0.k4ii872obco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
